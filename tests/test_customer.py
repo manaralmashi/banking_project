dc886e847a0647a1f72c4ed56494fc31b442d880
Manar Al Mashi <manar.almashii@gmail.com>
2025-09-25 01:53:19 +0300

    add test_has_checking_account() and test_has_savings_account() methods

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -13,6 +13,11 @@ class TestCustomer(unittest.TestCase):
         self.customer5 = Customer('5', 'Jana', 'khaled', 'Jana khaled')
         self.customer6 = Customer('6', 'Salma', 'Yosuf', 'Salma@12345')
         
+        self.customer_with_checking = Customer('7', 'Alaa', 'Faisal', 'Alaa@1234', balance_checking = 200)
+        self.customer_with_savings = Customer('8', 'Hala', 'Saad', 'Hala@1234', balance_savings = 1500)
+        self.customer_with_both = Customer('9', 'Hasan', 'Yaser', 'Hasan@1234', balance_checking = 3000, balance_savings = 5000)
+        self.customer_with_none = Customer('10', 'Muna', 'Saif', 'Muna@1234')
+        
     # [2]. testing methods
     def test_is_valid_password(self):
         # print('-------- on test_is_valid_password()--------') # for testing
@@ -29,10 +34,22 @@ class TestCustomer(unittest.TestCase):
         self.assertEqual(self.customer3.get_fullname(), "Fatima Noor")
         
     def test_is_equal_password(self):
-        self.assertTrue(Customer.is_equal_password("Hala@1234", "Hala@1234"))
-        self.assertTrue(Customer.is_equal_password("", ""))
-        self.assertFalse(Customer.is_equal_password("Manar@123", "Maanr@123"))
-        self.assertFalse(Customer.is_equal_password("Saad$12345", "saad$12345"))
+        self.assertTrue(Customer.is_equal_password("Hala@1234", "Hala@1234")) # True
+        self.assertTrue(Customer.is_equal_password("", "")) # True
+        self.assertFalse(Customer.is_equal_password("Manar@123", "Maanr@123")) # False
+        self.assertFalse(Customer.is_equal_password("Saad$12345", "saad$12345")) # False
+        
+    def test_has_checking_account(self):
+        self.assertTrue(self.customer_with_checking.has_checking_account()) # True
+        self.assertFalse(self.customer_with_savings.has_checking_account()) # False
+        self.assertTrue(self.customer_with_both.has_checking_account()) # True
+        self.assertFalse(self.customer_with_none.has_checking_account()) # False
+
+    def test_has_savings_account(self):
+        self.assertFalse(self.customer_with_checking.has_savings_account()) # False
+        self.assertTrue(self.customer_with_savings.has_savings_account()) # True
+        self.assertTrue(self.customer_with_both.has_savings_account()) # True
+        self.assertFalse(self.customer_with_none.has_savings_account()) # False
         
     # [3]. close - (if i want run something after test)
     def tearDown(self):
